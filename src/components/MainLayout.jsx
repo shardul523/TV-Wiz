615add3e9ae590d5095ede1e9b735075737ccda6
Shardul Sisodiya <98705625+shardul523@users.noreply.github.com>
2023-07-07 16:25:00 +0530

    Changed the title of the app

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -5,7 +5,7 @@ import Navigation from './Navigation';
 export default function MainLayout() {
     return (
         <>
-            <h1>Box Office App</h1>
+            <h1>TV Wiz App</h1>
             <Navigation />
             <Outlet />
         </>
